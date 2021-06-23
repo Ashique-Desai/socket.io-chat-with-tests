7d15666a0f87d102a1e65d3f105c9e25b7162cc4
Ashique-Desai <desai.ashique@gmail.com>
2021-06-23 16:54:37 +0530

    New Directory structure

---
+++ client/src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+import "./Home.css";
+
+function Home() {
+    return(
+        <div>Home</div>
+    )
+}
+
+export default Home
